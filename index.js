d94df7a0b697a93308ab6d4db70e278a439d1528
chaimass6 <chaitu.6914@gmail.com>
2025-02-07 20:37:10 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 gsap.fromTo("#svg", { strokeDashoffset: 4500 }, {
     strokeDashoffset: 0,
     duration: 4,
-    ease: "power2.inOut"
+    ease: "power4.inOut"
 });
 
 // Fade-in Text Animation
@@ -10,7 +10,7 @@ gsap.fromTo("#logoText", { opacity: 0 }, {
     opacity: 1,
     duration: 3,
     delay: 3,
-    ease: "power2.out"
+    ease: "power4.out"
 });
 
 // Hide Loading Page Animation
@@ -18,7 +18,7 @@ gsap.to(".lpage", {
     opacity: 0,
     duration: 1.5,
     delay: 4,
-    ease: "power2.out"
+    ease: "power4.out"
 });
 // Wait for the logo animation to complete, then redirect
 gsap.to("#svg", {
