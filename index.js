b5f18a6d39db1aa73d8a27b07e8909e32ca0d5cf
chaitanya <131352623+chaimass6@users.noreply.github.com>
2025-02-14 20:58:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 // SVG Animation
 gsap.fromTo("#svg", { strokeDashoffset: 4500 }, {
     strokeDashoffset: 0,
-    duration: 4,
+    duration: 5,
     ease: "power4.inOut"
 });
-
 // Fade-in Text Animation
 gsap.fromTo("#logoText", { opacity: 0 }, {
     opacity: 1,
-    duration: 3,
+    duration: 5,
     delay: 3,
     ease: "power4.out"
 });
-
 // Hide Loading Page Animation
 gsap.to(".lpage", {
     opacity: 0,
-    duration: 1.5,
-    delay: 4,
+    duration: 3,
+    delay: 3,
     ease: "power4.out"
-});
-// Wait for the logo animation to complete, then redirect
-gsap.to("#svg", {
-    strokeDashoffset: 0,
-    duration: 4.1,  // Adjust based on your animation duration
-    onComplete: function() {
-        setTimeout(() => {
-            window.location.href = "chaitu.html"; // Change 'main.html' to your actual main page filename
-        }, 1000); // Adjust delay if needed
-    }
-});
+ });
+// gsap.to("#svg", {
+//     strokeDashoffset: 0,
+//     duration: 4.1,  
+//     onComplete: function() {
+//         setTimeout(() => {
+//             window.location.href = "chaitu.html"; // Change 'main.html' to your actual main page filename
+//         }, 1000); // Adjust delay if needed
+//     }
+// });
 
