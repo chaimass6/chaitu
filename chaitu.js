66d91ad34a68db7a859dc1022998739181bc5999
chaimass6 <chaitu.6914@gmail.com>
2025-02-18 20:19:22 +0530

    ok

--- chaitu.js
+++ chaitu.js
@@ -90,4 +90,36 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add class to start the marquee animation
     profileContainer.classList.add('in-view');
   });
-  
+
+  let menuIcon = document.querySelector('#menu-icon');
+  let nav = document.querySelector('.navbar');
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-times');
+    nav.classList.toggle('active');
+  }
+
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('header nav a');
+  window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+      };
+      
+  });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+    menuIcon.classList.remove('fa-times');
+    nav.classList.remove('active');
+
+  } 
+
