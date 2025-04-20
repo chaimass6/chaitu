7c52cfe93223cab1bc3d48b6689f1bb3e891986b
chaimass6 <chaitu.6914@gmail.com>
2025-04-20 12:09:32 +0530

    ok

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ gsap.to("#svg", {
     duration: 3.5,  
     onComplete: function() {
         setTimeout(() => {
-            window.location.href = "index.html"; 
+
+            window.location.href = "chaitu.html"; 
         }, 1000); // Adjust delay if needed
     }
 });
