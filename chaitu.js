02eb5571bf98e824f7374ff04f5eba8755f30ae6
chaitanya <131352623+chaimass6@users.noreply.github.com>
2025-05-04 18:19:03 +0530

    Update chaitu.js

--- chaitu.js
+++ chaitu.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     function activateLink() {
       let scrollY = window.scrollY;
       let activeSet = false;
-  
       sections.forEach((section) => {
         let sectionHeight = section.offsetHeight;
         let sectionTop = section.offsetTop - 50;
